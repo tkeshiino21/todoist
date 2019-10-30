0c603fd9539407ab099d53cbbc44c74beeb318db
inouejoushi <takeshitoras.79@gmail.com>
2019-10-31 02:12:46 +0900

    prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.scss';
-import { Header } from './components/layout/Header'
-import { Sidebar }from './components/layout/Sidebar'
-import { Content } from './components/layout/Content'
+import React from "react";
+import "./App.scss";
+import { Header } from "./components/layout/Header";
+import { Sidebar } from "./components/layout/Sidebar";
+import { Content } from "./components/layout/Content";
 
 function App() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <p>Hello there!</p>
-          </header>
-          <Header />
-          <Sidebar />
-          <Content />
-        </div>
-      );
-    }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello there!</p>
+      </header>
+      <Header />
+      <Sidebar />
+      <Content />
+    </div>
+  );
+}
 
 export default App;
