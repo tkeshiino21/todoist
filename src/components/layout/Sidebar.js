01a9fde860bc5cceead68bc9abb7433bd0b62fe5
inouejoushi <takeshitoras.79@gmail.com>
2019-10-31 13:09:40 +0900

    header

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,32 +1,48 @@
-import React from 'react'
-import { FaCalendar, FaInbox, FaChevronDown, FaRegCalendarAlt } from 'react-icons/fa';
+import React from "react";
+import {
+  FaCalendar,
+  FaInbox,
+  FaChevronDown,
+  FaRegCalendarAlt
+} from "react-icons/fa";
 
 export const Sidebar = () => {
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul>
-        <li data-testid="inbox" dclassName="inbox"><span>
-          <FaInbox />Inbox
-        </span></li>
-        <li data-testid="Today" className="today"><span>
-          <FaRegCalendarAlt />Calender
-        </span></li>
-        <li><span>
-          <FaChevronDown />Down
-        </span></li>
-        <li><span>
-          <FaChevronDown />Next 7 days
-        </span></li>
+        <li data-testid="inbox" dclassName="inbox">
+          <span>
+            <FaInbox />
+            Inbox
+          </span>
+        </li>
+        <li data-testid="Today" className="today">
+          <span>
+            <FaRegCalendarAlt />
+            Today
+          </span>
+        </li>
+        <li data-testid="7days" className="7days">
+          <span>
+            <FaCalendar />
+            Next 7 days
+          </span>
+        </li>
+        <li>
+          <span>
+            <FaChevronDown />
+          </span>
+        </li>
       </ul>
 
       <div className="sidebar__middle">
-        <span><FaChevronDown /></span>
         <h2>Projects</h2>
       </div>
 
       <ul className="sidebar__projects">
-        <p>Add your project!</p>
+        <li>project will be here</li>
       </ul>
+      <p>Add your project!</p>
     </div>
-  )
-}
+  );
+};
